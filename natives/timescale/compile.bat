059fbb7880bdabedc26cb6b6ded1df256e54ba0a
Natan <natanbc@users.noreply.github.com>
2018-04-30 19:50:49 -0300

    fix timescale compile script for windows

--- natives/timescale/compile.bat
+++ natives/timescale/compile.bat
@@ -1,1 +1,1 @@
-cl /Ox /LD /EHsc /Fe:timescale.dll /I "%JAVA_HOME%\include\win32" /I "%JAVA_HOME%\include" *.cpp soundtouch\*.cpp
+cl /Ox /LD /EHsc /Fe:timescale.dll /I "%JAVA_HOME%\include\win32" /I "%JAVA_HOME%\include" %~dp0\*.cpp %~dp0\soundtouch\*.cpp
