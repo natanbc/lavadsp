fbdfcb9015cb7e8eb60b1b96963cfcf183836283
Natan <natanbc@users.noreply.github.com>
2018-05-02 22:35:08 -0300

    add cleanlogs argument support for non windows build scripts

--- natives/compile-all-mac.sh
+++ natives/compile-all-mac.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 deploy=0
+cleanlogs=0
 
 while [ "$1" != "" ]; do
     if [ "$1" == "deploy" ]; then
         deploy=1
+    fi
+    if [ "$1" == "cleanlogs" ]; then
+        cleanlogs=1
     fi
     shift
 done
@@ -18,4 +22,8 @@ done
 
 if [ deploy == 1 ]; then
     "cp *.dylib ../src/main/resources/natives/darwin/"
+fi
+
+if [ cleanlogs == 1 ]; then
+    "rm *.log"
 fi
